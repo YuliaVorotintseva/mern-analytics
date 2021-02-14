280f43ddfb24003765f17e5d2b3b569a9f32bef1
Yulia Vorotintseva <yulia.vorotintseva@gmail.com>
2021-02-14 17:59:16 +0600

    Added auth route

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ const config = require('config')
 const mongoose = require('mongoose')
 
 const app = express()
-const PORT = config.get('port') || 5000
+app.use('/api/auth', require('./routes/auth.route'))
 
+const PORT = config.get('port') || 5000
 async function start() {
-    try {
-        await mongoose.connect(config.get('mongoUri'), {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        })
-        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
-    } catch(error) {
-        console.log('Server error', error.message)
-        process.exit(1)
-    }
+	try {
+		await mongoose.connect(config.get('mongoUri'), {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true
+		})
+		app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+	} catch (error) {
+		console.log('Server error', error.message)
+		process.exit(1)
+	}
 }
 start()
