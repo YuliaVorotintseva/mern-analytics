0fa3012781bcc38545adf286b14d7f77abe42267
Yulia Vorotintseva <yulia.vorotintseva@gmail.com>
2021-02-15 23:42:26 +0600

    Added http custom hook, added proxy to port 5000

--- client/src/pages/auth/AuthPage.js
+++ client/src/pages/auth/AuthPage.js
@@ -1,41 +1,59 @@
 import React, { useState } from 'react'
+import { useHTTP } from '../../hooks/http.hook'
 import './AuthPage.css'
 
 export const AuthPage = () => {
-  const [form, setForm] = useState({email: '', password: ''})
+  const { loading, error, request } = useHTTP()
+  const [form, setForm] = useState({ email: '', password: '' })
 
-  const changeHandler = event => setForm({...form, [event.target.name]: event.target.value})
+  const changeHandler = event => setForm({ ...form, [event.target.name]: event.target.value })
+  const registerHandler = async () => {
+    try {
+      const data = await request('/api/auth/register', 'POST', { ...form })
+    } catch (error) { }
+  }
 
   return (
     <div className="row">
       <div className="col s6 offset-s3">
         <h1>LINK SHORTENING</h1>
-        <div class="card amber lighten-5">
-          <div class="card-content white-text">
-            <span class="card-title custom-span">Authorization</span>
+        <div className="card amber lighten-5">
+          <div className="card-content white-text">
+            <span className="card-title custom-span">Authorization</span>
 
             <div>
-              <div class="input-field">
-                <input 
+              <div className="input-field">
+                <input
                   placeholder="Email..." id="email" type="text" name="email"
                   onChange={changeHandler}
                 />
-                <label for="email">Email</label>
+                <label htmlFor="email">Email</label>
               </div>
 
-              <div class="input-field">
+              <div className="input-field">
                 <input
                   placeholder="Password..." id="password" type="password" name="password"
                   onChange={changeHandler}
                 />
-                <label for="password">Password</label>
+                <label htmlFor="password">Password</label>
               </div>
             </div>
           </div>
 
-          <div class="card-action">
-            <button className="btn amber darken-1 btn-custom">LOG UP</button>
-            <button className="btn teal lighten-5 black-text btn-custom">LOG IN</button>
+          <div className="card-action">
+            <button
+              className="btn amber darken-1 btn-custom"
+              disabled={loading}
+            >
+              LOG UP
+            </button>
+            <button
+              className="btn teal lighten-5 black-text btn-custom"
+              onClick={registerHandler}
+              disabled={loading}
+            >
+              LOG IN
+            </button>
           </div>
         </div>
       </div>
