49fc33a0547a52013a3c59050c5e719091a162c8
Yulia Vorotintseva <yulia.vorotintseva@gmail.com>
2021-03-18 23:29:37 +0600

    Added production json

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.route'))
 app.use('/api/link', require('./routes/link.route'))
 app.use('/to', require('./routes/redirect.route'))
+
+if (process.env.NODE_ENV === 'production'){
+	app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+	app.get('*', (request, response) => response.sendFile(path.join(__dirname, 'client', 'build', 'index.html')))
+}
 
 const PORT = config.get('port') || 3000
 async function start() {
---
+++ config/production.js
@@ -0,0 +1,6 @@
+{
+  "port": 5000,
+  "jwtSecret": "mern",
+  "mongoUri": "mongodb+srv://yulia:24E6A4f78@cluster0.frovy.mongodb.net/app?retryWrites=true&w=majority",
+  "baseUrl": "http://localhost:5000"
+}
